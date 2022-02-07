af2a28eaca7f35d1e8e6aabff4ba64f14e50906b
rbouchet <rbouchet@C11-06.sio.local>
2022-02-07 15:35:15 +0100

    completion de la classe ArrayList

--- agenda/src/main/java/utils/List.java
+++ agenda/src/main/java/utils/List.java
@@ -1,5 +1,12 @@
 package utils;
 
-public interface List {
+public interface List<Type> {
 
+	void add(Type element);
+	void remove(Type element);
+	void remove(int index);
+	Type get(int index);
+	int length();
+	boolean contains(Type element);
+	int indexOf(Type element);
 }
